df767d3572944998c2a0991d53d4520d39d11ed9
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2020-05-03 20:47:54 +0300

    + 3.1

--- src/kotlin/ru/xpendence/kotlintasksstepik/Arrays.kt
+++ src/kotlin/ru/xpendence/kotlintasksstepik/Arrays.kt
@@ -6,8 +6,15 @@ package ru.xpendence.kotlintasksstepik
  * @since 30.04.2020
  * @author Вячеслав Чернышов
  */
-fun main(args: Array<String>) {
-    var array = readLine()!!.split(" ").map { it.toDouble() }
-    print(array.average())
-    array.sorted().
+
+//Необходимо разработать программу для поиска среднего и медианного значений списка действительных чисел со знаком,
+//подаваемого на вход программы. Медианным значением считается такое число выборки, что ровно половина из элементов
+//выборки больше него, а другая половина меньше него. Если в выборке чётное число элементов, медиана вычисляется как
+//полусумма двух центральных элементов в упорядоченной выборке.
+//Вход: список с n элементами.
+//Выход: два значения (среднее, медиана)
+fun countAverageAndMedian(args: Array<String>) {
+    val array = readLine()!!.split(" ").map { it.toDouble() }
+    println(array.average())
+    array.sorted().let { (it[it.size / 2] + it[(it.size - 1) / 2]) / 2 }.let { println(it) }
 }
